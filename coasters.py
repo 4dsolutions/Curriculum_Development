0d011b292dc0e12c86ffdb24a5386806dda36beb
4dsolutions <kirby.urner@gmail.com>
2020-04-19 07:51:37 -0700

    pulling it together

---
+++ coasters.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 18:44:57 2020
+
+@author: mac
+"""
+
+import pandas as pd
+
+
---
+++ statsfun.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 17:57:28 2020
+
+@author: StatsGnewt
+"""
+import pandas as pd
+
+from math import factorial
+
+def choose(n, k):
+    return factorial(n)//(factorial(k)*factorial(n-k))
+
+def sum_binom(n, x):
+    return sum(choose(n,k) * x**k for k in range(0, n+1))
+
+def pascal(n):
+    return [choose(n,k) for k in range(0,n+1)]
+
+def get_coasters():
+    coasters = pd.read_csv("./data/roller_coasters.csv")
+    return coasters
